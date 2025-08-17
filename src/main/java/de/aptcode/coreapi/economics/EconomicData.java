031fdb568d52c0e11cb61f1a995599e73f8acb2d
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-08-17 16:25:22 +0200

    Market Data
    Added marketdata and market item

---
+++ src/main/java/de/aptcode/coreapi/economics/EconomicData.java
@@ -0,0 +1,15 @@
+package de.aptcode.coreapi.economics;
+
+import de.aptcode.coreapi.data.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class EconomicData extends Data<EconomicData> {
+
+
+
+}
--- src/main/java/de/aptcode/coreapi/player/PlayerRepositoryInterface.java
+++
@@ -1,7 +0,0 @@
-package de.aptcode.coreapi.player;
-
-import de.aptcode.coreapi.interfaces.RepositoryInterface;
-
-public interface PlayerRepositoryInterface extends RepositoryInterface<String, PlayerData> {
-
-}
