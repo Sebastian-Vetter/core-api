9bf8db69f10b8364175822be6133aadd9dd51cfa
MrSpigot <220609166+Sebastian-Vetter@users.noreply.github.com>
2025-09-25 16:08:39 +0200

    Added WorkerEvents.kt

---
+++ src/main/kotlin/de/aptcode/core/api/worker/WorkerEvents.kt
@@ -0,0 +1,13 @@
+package de.aptcode.core.api.worker
+
+enum class WorkerEvents {
+
+    START_SERVER,
+    STOP_SERVER,
+    RESTART_SERVER,
+    DELETE_SERVER,
+    CREATE_SERVER_GROUP,
+    DELETE_SERVER_GROUP,
+    HEARTBEAT
+
+}
